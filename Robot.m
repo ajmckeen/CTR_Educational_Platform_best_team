047a78de08a2509a21b33eb212569478867bd2ac
ajmckeen <ajmckeen@wpi.edu>
2024-10-02 12:55:28 -0400

    maybe it works

--- Robot.m
+++ Robot.m
@@ -64,13 +64,13 @@ classdef Robot < handle
             self.num_tubes
             
             % Initialize an array to hold the transition points
-            transitionPoints = zeros(1, 2 * nTubes);
+            transitionPoints = zeros(1, 2 * self.num_tubes);
             
             % Calculate the transition points (start and end of curved section)
-            for i = 1:nTubes
+            for i = 1:self.num_tubes
                 transitionPoints(2*i - 1) = rho(i);         % Start of curved section
                 %transitionPoints(2*i) = rho(i) + d(i);      % End of curved section
-                transitionPoints(2*i) = rho(i) + tubes(i).d
+                transitionPoints(2*i) = rho(i) + self.tubes(i).d;
             end
             
             % Sort the transition points in ascending order
@@ -90,75 +90,6 @@ classdef Robot < handle
 
             for i=1:self.num_tubes
                 theta(i) = deg2rad(q_var(i+self.num_tubes));
-            end
-        end
-
-
-        function curvatureInputs = prepareLinkCurvatureInputs(E, OD, ID, k, theta, rho, d)
-            % Determine which tubes are present (and at what curvatures) in all of the links
-        
-            % E: Young's Modulus of the material for each tube [N/m^2]
-            % OD: Outer diameters of the tubes [m]
-            % ID: Inner diameters of the tubes [m]
-            % k: Precurvatures of the tubes [m^-1]
-            % theta: Rotations of the tubes [rad]
-            % rho: Starting points of the curved sections [m]
-            % d: Length of the curved sections for each tube [m]
-            
-            % Number of tubes
-            nTubes = length(rho);
-            
-            % Calculate the transition points (start and end of curved section)
-            transitionPoints = zeros(1, 2 * nTubes);
-            for i = 1:nTubes
-                transitionPoints(2*i - 1) = rho(i);         % Start of curved section
-                transitionPoints(2*i) = rho(i) + d(i);      % End of curved section
-            end
-            
-            % Sort the transition points in ascending order
-            sortedTransitionPoints = sort(transitionPoints);
-            
-            % Initialize a cell array to store all the inputs for each link
-            curvatureInputs = cell(1, length(sortedTransitionPoints) - 1);
-            
-            % Loop through each link (between consecutive transition points)
-            for linkIdx = 1:(length(sortedTransitionPoints) - 1)
-                % Determine the current link start and end
-                linkStart = sortedTransitionPoints(linkIdx);
-                linkEnd = sortedTransitionPoints(linkIdx + 1);
-                
-                % Initialize arrays for E, OD, ID, k, and theta for the active tubes in this link
-                activeE = [];
-                activeOD = [];
-                activeID = [];
-                activeK = [];
-                activeTheta = [];
-                
-                % Loop through each tube and check if it's active in this link
-                for tubeIdx = 1:nTubes
-                    tubeStart = rho(tubeIdx);
-                    tubeEnd = rho(tubeIdx) + d(tubeIdx);
-                    
-                    % A tube is active from the base of the robot (not just after rho)
-                    if linkStart >= 0 && linkEnd <= tubeEnd
-                        % Include the tube
-                        activeE = [activeE, E(tubeIdx)];
-                        activeOD = [activeOD, OD(tubeIdx)];
-                        activeID = [activeID, ID(tubeIdx)];
-                        activeTheta = [activeTheta, theta(tubeIdx)];
-                        
-                        % Assign pre-curvature: 0 if the link is before the tube's rho, actual value if after
-                        if linkStart < tubeStart
-                            activeK = [activeK, 0];  % No pre-curvature before rho
-                        else
-                            activeK = [activeK, k(tubeIdx)];  % Actual pre-curvature after rho
-                        end
-                    end
-                end
-                
-                % Store the active tube information for this link
-                curvatureInputs{linkIdx} = struct('E', activeE, 'OD', activeOD, 'ID', activeID, ...
-                                                  'k', activeK, 'theta', activeTheta);
             end
         end
 
@@ -204,34 +135,148 @@ classdef Robot < handle
         % Should return phi (1 x j vector, where j is num links)
         % and K (1 x j vector)
         function [phi,K] = calculate_phi_and_kappa(self, theta, rho)
+            % Pre-allocate arrays for the properties of each tube
+            E = zeros(1, self.num_tubes);
+            OD = zeros(1, self.num_tubes);
+            ID = zeros(1, self.num_tubes);
+            k = zeros(1, self.num_tubes);
+            d = zeros(1, self.num_tubes);
             for tubeIdx = 1:self.num_tubes
-                E = self.tubes(tubeIdx).E;
-                OD = self.tubes(tubeIdx).od;
-                ID = self.tubes(tubeIdx).id;
-                k = self.tubes(tubeIdx).k;
+                E(tubeIdx) = self.tubes(tubeIdx).E;
+                OD(tubeIdx) = self.tubes(tubeIdx).od;
+                ID(tubeIdx) = self.tubes(tubeIdx).id;
+                k(tubeIdx) = self.tubes(tubeIdx).k;
                 alpha = theta;
                 rho = rho;
-                d = self.tubes(tubeIdx).d;
+                d(tubeIdx) = self.tubes(tubeIdx).d;
 
             end
+
+
+            function curvatureInputs = prepareLinkCurvatureInputs(E, OD, ID, k, theta, rho, d)
+                % Determine which tubes are present (and at what curvatures) in all of the links
+            
+                % E: Young's Modulus of the material for each tube [N/m^2]
+                % OD: Outer diameters of the tubes [m]
+                % ID: Inner diameters of the tubes [m]
+                % k: Precurvatures of the tubes [m^-1]
+                % theta: Rotations of the tubes [rad]
+                % rho: Starting points of the curved sections [m]
+                % d: Length of the curved sections for each tube [m]
+                
+                % Number of tubes
+    
+                disp('hi')
+                nTubes = length(rho);
+                
+                % Calculate the transition points (start and end of curved section)
+                transitionPoints = zeros(1, 2 * nTubes);
+                for i = 1:nTubes
+                    transitionPoints(2*i - 1) = rho(i);         % Start of curved section
+                    transitionPoints(2*i) = rho(i) + d(i);      % End of curved section
+                end
+                
+                % Sort the transition points in ascending order
+                sortedTransitionPoints = sort(transitionPoints);
+                
+                % Initialize a cell array to store all the inputs for each link
+                curvatureInputs = cell(1, length(sortedTransitionPoints) - 1);
+                
+                % Loop through each link (between consecutive transition points)
+                for linkIdx = 1:(length(sortedTransitionPoints) - 1)
+                    % Determine the current link start and end
+                    linkStart = sortedTransitionPoints(linkIdx);
+                    linkEnd = sortedTransitionPoints(linkIdx + 1);
+                    
+                    % Initialize arrays for E, OD, ID, k, and theta for the active tubes in this link
+                    activeE = [];
+                    activeOD = [];
+                    activeID = [];
+                    activeK = [];
+                    activeTheta = [];
+                    
+                    % Loop through each tube and check if it's active in this link
+                    for tubeIdx = 1:nTubes
+                        tubeStart = rho(tubeIdx);
+                        tubeEnd = rho(tubeIdx) + d(tubeIdx);
+                        
+                        % A tube is active from the base of the robot (not just after rho)
+                        if linkStart >= 0 && linkEnd <= tubeEnd
+                            % Include the tube
+                            activeE = [activeE, E(tubeIdx)];
+                            activeOD = [activeOD, OD(tubeIdx)];
+                            activeID = [activeID, ID(tubeIdx)];
+                            activeTheta = [activeTheta, theta(tubeIdx)];
+                            
+                            % Assign pre-curvature: 0 if the link is before the tube's rho, actual value if after
+                            if linkStart < tubeStart
+                                activeK = [activeK, 0];  % No pre-curvature before rho
+                            else
+                                activeK = [activeK, k(tubeIdx)];  % Actual pre-curvature after rho
+                            end
+                        end
+                    end
+                    
+                    % Store the active tube information for this link
+                    curvatureInputs{linkIdx} = struct('E', activeE, 'OD', activeOD, 'ID', activeID, ...
+                                                      'k', activeK, 'theta', activeTheta);
+                end
+            end
+
+            function [chi,gamma] = linkcurvature(E, OD, ID, k, theta)
+                % E: Young's Modulus of the material for each tube [N/m^2]
+                % OD: Outer diameters of the tubes [m]
+                % ID: Inner diameters of the tubes [m]
+                % k: Precurvatures of the tubes [m^-1]
+                % theta: Rotations of the tubes [rad]
+                
+            
+                % Number of tubes
+                nTubes = length(OD);
+                
+                % Initialize the second moment of inertia array
+                I = zeros(1, nTubes);
+                
+                % Calculate the second moment of inertia for each tube
+                for i = 1:nTubes
+                    I(i) = (pi/64) * (OD(i)^4 - ID(i)^4);
+                end
+                
+                % Initialize sums in the numerator and denominator for chi and gamma
+                sum_EIk_cosTheta = 0;
+                sum_EIk_sinTheta = 0;
+                sum_EI = 0;
+                
+                % Calculate the sums for chi and gamma
+                for i = 1:nTubes
+                    sum_EIk_cosTheta = sum_EIk_cosTheta + E(i) * I(i) * k(i) * cos(theta(i));
+                    sum_EIk_sinTheta = sum_EIk_sinTheta + E(i) * I(i) * k(i) * sin(theta(i));
+                    sum_EI = sum_EI + E(i) * I(i);
+                end
+                
+                % Calculate chi and gamma
+                chi = sum_EIk_cosTheta / sum_EI;
+                gamma = sum_EIk_sinTheta / sum_EI;
+            end
+
 
             curvatureInputs = prepareLinkCurvatureInputs(E, OD, ID, k, alpha, rho, d);
 
             % Initialize arrays to store chi and gamma for each link
-            chi_all = zeros(1, num_tubes)
-            gamma_all = zeros(1, num_tubes)
+            chi_all = zeros(1, self.num_tubes);
+            gamma_all = zeros(1, self.num_tubes);
 
             % Loop through each link and calculate chi and gamma
-            for linkIdx = 1:nLinks
+            for linkIdx = 1:self.num_tubes
                 % Get the curvature inputs for the current link
                 linkData = curvatureInputs{linkIdx};
                 
                 % Extract the relevant parameters for the link
-                E_link = linkData.E
-                OD_link = linkData.OD
-                ID_link = linkData.ID
-                k_link = linkData.k
-                theta_link = linkData.theta
+                E_link = linkData.E;
+                OD_link = linkData.OD;
+                ID_link = linkData.ID;
+                k_link = linkData.k;
+                theta_link = linkData.theta;
                 
                 % Call linkcurvature to calculate chi and gamma for the current link
                 [chi, gamma] = linkcurvature(E_link, OD_link, ID_link, k_link, theta_link);
@@ -242,22 +287,21 @@ classdef Robot < handle
             end
 
             % Initialize arrays to store phi and K for each link
-            phi = zeros(1, num_tubes);
-            K = zeros(1, num_tubes);
+            phi = zeros(1, size(self.lls,2));
+            K = zeros(1, size(self.lls,2));
 
             % Loop through each link and calculate phi and K
             phi_sum = 0;
-            for j = 1:nLinks
+            for j = 1:self.num_tubes
                 % Calculate the link rotation phi_j
-                gamma_all(j)
-                chi_all(j)
                 next_phi = atan2(gamma_all(j), chi_all(j));
-                phi(j) = next_phi - phi_sum
+                phi(j) = next_phi - phi_sum;
                 phi_sum = phi_sum + phi(j);
                 
                 % Calculate the link curvature kappa_j
                 K(j) = sqrt(chi_all(j)^2 + gamma_all(j)^2);
             end
+            
         end
 
         % Take in all robot dependent parameters (lls, phi, kappa) and
@@ -265,9 +309,41 @@ classdef Robot < handle
         % Returns a 4x4 transformation matrix from base frame to end
         % effector
         function T = calculate_transform(self, s, phi, K)
-            % ********************************************************
-            %                          TODO
-            % ********************************************************
+            s
+            phi
+            K
+            % Initialize T as the identity matrix (4x4)
+            T = eye(4);
+            
+            % Loop through each link]
+            length(s)
+            for i = 1:length(s)
+                % Get the i-th link parameters
+                l_i = s(i);   % Link length
+                phi_i = phi(i);  % Orientation angle
+                k_i = K(i);    % Curvature
+
+                % Calculate the transformation matrix for the i-th link
+                if k_i == 0
+                    Ti_0 = [
+                    cos(phi_i)*cos(k_i*l_i),  -sin(phi_i),   cos(phi_i)*sin(k_i*l_i),  0;
+                    sin(phi_i)*cos(k_i*l_i),  cos(phi_i),    sin(phi_i)*sin(k_i*l_i),  0;
+                    -sin(k_i*l_i),            0,            cos(k_i*l_i),              l_i;
+                    0,                        0,            0,                         1
+                    ]
+                else
+                    Ti_0 = [
+                    cos(phi_i)*cos(k_i*l_i),  -sin(phi_i),   cos(phi_i)*sin(k_i*l_i),  (cos(phi_i)*(1 - cos(k_i*l_i)))/k_i;
+                    sin(phi_i)*cos(k_i*l_i),  cos(phi_i),    sin(phi_i)*sin(k_i*l_i),  (sin(phi_i)*(1 - cos(k_i*l_i)))/k_i;
+                    -sin(k_i*l_i),            0,            cos(k_i*l_i),              sin(k_i*l_i)/k_i;
+                    0,                        0,            0,                         1
+                    ];
+                end 
+
+                
+                % Multiply the accumulated transformation matrix by the current one
+                T = T * Ti_0;
+            end
         end
     end
 end
--- perform_kinematics.m
+++ perform_kinematics.m
@@ -7,21 +7,21 @@ tube3 = Tube(1.726*10^-3, 1.98*10^-3, 1/29, 250*10^-3, 50*10^-3, 1935*10^6);
 
 % TODO You will need to uncomment one of the below lines, depending if you
 % are testing a 2-tube or 3-tube case
-tubes = [tube1, tube2];
-% tubes = [tube1, tube2, tube3];
+%tubes = [tube1, tube2];
+tubes = [tube1, tube2, tube3];
 
-robot = Robot(tubes, false);
+robot = Robot(tubes);
 
 % q values for testing with two tubes
-q_var = [0, 0, 0, 0; 
-         20, 50, 45, -45; 
-         30, 35, -35, 20];
+% q_var = [0, 0, 0, 0; 
+%          20, 50, 45, -45; 
+%          30, 35, -35, 20];
 
 
 % q values for testing with three tubes
-% q_var = [0, 0, 0, 0, 0, 0;
-%          20, 50, 70, 45, -45, 45; 
-%          30, 35, 50, -35, 20, -10];
+ q_var = [0, 0, 0, 0, 0, 0;
+          20, 50, 70, 45, -45, 45; 
+          30, 35, 50, -35, 20, -10];
 
 T = {};
 for i = 1:size(q_var,1)
